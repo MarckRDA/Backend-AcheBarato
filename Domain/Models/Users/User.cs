0050e6c2a04379f9647499539b17fb1e78bceac4
MarckRDA <marcos.edo132@gmail.com>
2020-12-23 19:25:08 -0300

    Moved from models to models.Users

---
+++ Domain/Models/Users/User.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Domain.Models.Products;
+
+namespace Domain.Models.Users
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Product> WishListProducts { get; set; }
+
+        private bool ValidateEmail()
+        {
+            return Regex.IsMatch(
+                Email,
+                @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+                RegexOptions.IgnoreCase
+            );
+        }
+
+        private bool ValidateName()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                return false;
+            }
+
+            var words = Name.Split(' ');
+            if (words.Length < 2)
+            {
+                return false;
+            }
+
+            foreach (var word in words)
+            {
+                if (word.Trim().Length < 2)
+                {
+                    return false;
+                }
+                if (word.Any(x => !char.IsLetter(x)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public (IList<string> errors, bool isValid) Validate()
+        {
+            var errors = new List<string>();
+            if (!ValidateName())
+            {
+                errors.Add("Nome inválido.");
+            }
+
+            if (!ValidateEmail())
+            {
+                errors.Add("Email inválido.");
+            }
+
+            return (errors, errors.Count == 0);
+        }
+    }
+}
