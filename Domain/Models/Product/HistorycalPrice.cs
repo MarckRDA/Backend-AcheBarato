3748dd9809cd8de0d41e6f898aa624720fe5c48e
MarckRDA <marcos.edo132@gmail.com>
2020-12-23 19:25:25 -0300

    Update some properties

--- Domain/Models/Product/HistorycalPrice.cs
+++ Domain/Models/Product/HistorycalPrice.cs
@@ -1,7 +1,11 @@
+using System;
 namespace Domain.Models.Products
 {
     public class HistorycalPrice
     {
-        
+        public Guid HistorycalPriceId { get; set; }
+        public DateTime DateOfPrice { get; set; }
+        public Product Product { get; set; }
+        public double Price { get; set; }
     }
 }
--- Domain/Models/Product/Product.cs
+++ Domain/Models/Product/Product.cs
@@ -7,6 +7,8 @@ namespace Domain.Models.Products
         public string ProductIdMLB { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string ImgLink { get; set; }
+        public string LinkRedirectShop { get; set; }
         public Cathegory cathegory { get; set; }
         public HistorycalPrice HistorycalPrices { get; set; }
     }
