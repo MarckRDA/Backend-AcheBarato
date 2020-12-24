3070aa5fb5b8dc1e1172fff9dd874e428447c487
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-12-24 08:44:59 -0300

    Added some methods in Product

---
+++ Domain/Models/Product/ProductsRepository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Models.Products;
+using Domain.Models.Users;
+
+namespace Domain.src.Times
+{
+    public static class ProductsRepository
+    {
+        private static List<Product> products = new List<Product>();
+
+        public static void AddProducts(List<Product> products)
+        {
+            foreach (Product item in products)
+            {
+                products.Add(item);
+            }
+            
+        }
+        
+        public static Product GetProduct(string idMlProduct)
+        {
+            return products.FirstOrDefault(t => t.ProductIdMLB == idMlProduct);
+        }
+
+        public static void AddProduct(Product insertProduct)
+        {
+            products.Add(insertProduct);
+        }
+
+        public static void RemoveProduct(string idMlProduct)
+        {
+            var productToRemove = products.FirstOrDefault(t => t.ProductIdMLB == idMlProduct);
+            products.Remove(productToRemove);
+        }
+
+    }
+}
