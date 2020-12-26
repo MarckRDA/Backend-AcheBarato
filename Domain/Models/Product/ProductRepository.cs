ee58b7b865cd0bd589f511d3280f6e5247111a08
MarckRDA <marcos.edo132@gmail.com>
2020-12-26 12:33:37 -0300

    Added ProductsRepository.cs

---
+++ Domain/Models/Product/ProductRepository.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using Domain.Infra;
+
+namespace Domain.Models.Products
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IRepository<Product> _productRepository;
+
+        public ProductRepository(IRepository<Product> repository)
+        {
+            _productRepository = repository;
+        }
+
+        public void UpdateProductPrice(string idMLB, double newPrice)
+        {    
+            using (var db = new AcheBaratoContext())
+            {
+                var productToUpdate = db.Products.FirstOrDefault(product => product.ProductIdMLB == idMLB);
+
+                productToUpdate.Price = newPrice;
+
+                db.SaveChanges();
+            }
+        }
+        
+        public void add(Product product)
+        {
+            if (GetElement(product => product.ProductIdMLB == product.ProductIdMLB) == null)
+            {
+                return;
+            } 
+            _productRepository.add(product);
+        }
+
+        public Product GetElement(Func<Product, bool> predicate)
+        {
+
+            return _productRepository.GetElement(predicate);
+        }
+
+        public void UpdateHistoricalPrice(string idMLB, HistorycalPrice historicalprice)
+        {
+            using (var db = new AcheBaratoContext())
+            {
+                var productToUpdate = db.Products.FirstOrDefault(product => product.ProductIdMLB == idMLB);
+
+                productToUpdate.HistorycalPrices.Add(historicalprice);
+
+                db.SaveChanges();
+            }           
+        }
+
+        
+    }
+}
