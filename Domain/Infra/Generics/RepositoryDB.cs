6b30a2a1525a1009e8ca3eec36b604df2e133337
MarckRDA <marcos.edo132@gmail.com>
2020-12-27 22:41:20 -0300

    Updated name's file

---
+++ Domain/Infra/Generics/RepositoryDB.cs
@@ -0,0 +1,25 @@
+using System;
+using Domain.Models.Entities;
+
+namespace Domain.Infra.Generics
+{
+    public class RepositoryDB<T> : IRepository<T> where T : Entity
+    {
+        public void add(T entity)
+        {
+            using (var db = new AcheBaratoContext())
+            {
+                db.Add<T>(entity);
+                db.SaveChanges();
+            }
+        }
+
+        public T GetElement(Func<T, bool> predicate)
+        {
+            using (var db = new AcheBaratoContext())
+            {
+                return db.Find<T>(predicate);
+            }
+        }
+    }
+}
