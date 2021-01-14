e1be2e9c46e00e92bce4db99541b9dba924cef51
MarckRDA <marcos.edo132@gmail.com>
2021-01-14 17:06:11 -0300

    Added a DateTime Parameter in contructor

--- Domain/Models/HistorycalPrices/HistorycalPrice.cs
+++ Domain/Models/HistorycalPrices/HistorycalPrice.cs
@@ -7,10 +7,9 @@ namespace Domain.Models.HistorycalPrices
     {
         public DateTime DateOfPrice { get; private set; }
         public double PriceOfThatDay { get; private set; }
-        
-        public HistorycalPrice(double priceOfThatDay )
+        public HistorycalPrice(double priceOfThatDay, DateTime date )
         {
-            DateOfPrice = new DateTime();
+            DateOfPrice = date;
             PriceOfThatDay = priceOfThatDay;
         }
     }
