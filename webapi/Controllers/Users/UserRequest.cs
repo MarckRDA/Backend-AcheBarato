14bcb0e284e168e93ddc5130183993dcde80ba19
MarckRDA <marcos.edo132@gmail.com>
2021-01-14 20:38:21 -0300

    Added properties into the entity

--- webapi/Controllers/Users/UserRequest.cs
+++ webapi/Controllers/Users/UserRequest.cs
@@ -1,10 +1,13 @@
 
-namespace WebAPI.Users
+using Domain.Models.Users;
+
+namespace webapi.Controllers.Users
 {
     public class UserRequest
     {
         public string Name { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public Profile Profile {get; set;}
     }
 }
