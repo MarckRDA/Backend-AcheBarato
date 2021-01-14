2ed85e3e54083fcd4ddd0f4cc3b02500cc5da9da
MarckRDA <marcos.edo132@gmail.com>
2021-01-14 17:07:24 -0300

    Added MonitorPrice Method

---
+++ webapi/Services/BackgroundService/ProductBackgroundTask.cs
@@ -0,0 +1,62 @@
+using System;
+using Domain.ApiMLBConnection.Consumers;
+using Domain.Models.HistorycalPrices;
+using Domain.Models.Products;
+using MongoDB.Driver;
+
+namespace webapi.Services.BackgroundService
+{
+    public class ProductBackgroundTask
+    {
+        private readonly IMongoCollection<Product> _collection;
+
+        public ProductBackgroundTask()
+        {
+            var database = new MongoClient("mongodb://root:AcheBaratoMongoDB2021!@localhost:27017").GetDatabase("AcheBarato");
+            _collection = database.GetCollection<Product>("Products");
+        }
+
+        public void PushProductsInDB()
+        {
+            try
+            {
+                var productsToPush = ApiMLB.PutProductsInBackGround();
+
+                foreach (var product in productsToPush)
+                {
+                    _collection.InsertManyAsync(product);
+                }
+            }
+            catch (System.Exception e)
+            {
+                throw new System.Exception($"erro: {e.Message}");
+            }
+
+        }
+
+        public void MonitorPriceProducts()
+        {
+            var productsInDB = _collection.AsQueryable().ToList();
+
+            foreach (var product in productsInDB)
+            {
+                var price = ApiMLB.FindWhetherProductPriceChanges(product.MLBId);
+                product.UpdateProductPrice(price);
+                product.AddHistoricalPrice(new HistorycalPrice(price, DateTime.Now));
+                try
+                {
+                    _collection.ReplaceOneAsync(
+                    p => p.id_product == product.id_product,
+                    product
+                );    
+                }
+                catch (System.Exception ex)
+                {
+                    
+                    throw new Exception($"Error in monitor price {ex.Message}");
+                }
+                
+            }
+        }
+    }
+}
