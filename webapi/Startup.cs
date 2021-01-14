70596566f731531c52ed0b81a60369c9fbb16703
MarckRDA <marcos.edo132@gmail.com>
2021-01-14 00:23:39 -0300

    Added hangfire configuration

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -3,12 +3,18 @@ using Domain.Infra.Repository;
 using Domain.Interfaces;
 using Domain.Services;
 using Domain.Settings;
+using Hangfire;
+using Hangfire.Mongo;
+using Hangfire.Mongo.Migration.Strategies;
+using Hangfire.Mongo.Migration.Strategies.Backup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using webapi.Services;
 
 namespace webapi
 {
@@ -26,27 +32,54 @@ namespace webapi
         {
             services.Configure<MongoSettings>(
                 Configuration.GetSection(nameof(MongoSettings)));
-            
+
             services.AddScoped<IMongoSettings>(sp =>
                 sp.GetRequiredService<IOptions<MongoSettings>>().Value);
-                       
+
             services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductServices, ProductServices>();
+
+
+           var mongoUrlBuilder = new MongoUrlBuilder("mongodb://root:AcheBaratoMongoDB2021!@localhost:27017");
+           var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+
+           services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMongoStorage(mongoClient, "AcheBarato", new MongoStorageOptions
+                {
+                    MigrationOptions = new MongoMigrationOptions
+                    {
+                        MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                        BackupStrategy = new CollectionMongoBackupStrategy()
+                    },
+                    Prefix = "hangfire.mongo",
+                    CheckConnection = true
+                }));
+
+                services.AddHangfireServer(serverOptions =>
+                {
+                    serverOptions.ServerName = "Hangfire.Mongo server 1";
+                });
 
             services.AddControllers();
             
             ProductMap.Configure();
 
+            //ApiMLB.PutProductsInBackGround();
+
             UserMap.Configure();
 
-            
+
         }
 
+       
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -57,13 +90,25 @@ namespace webapi
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseHangfireDashboard();
+
+            app.UseHangfireServer();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            InitProcess();
         }
 
-        
+        private void InitProcess()
+        {
+            var division = new Division();
+            RecurringJob.AddOrUpdate(() => division.DivisionRandom(), Cron.Minutely());
+        }
+
+
     }
 }
