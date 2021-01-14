2c4a34607ab6ed6e4e673fe8b7e00884c518fa67
MarckRDA <marcos.edo132@gmail.com>
2021-01-14 00:24:53 -0300

    Changed to test get 2k products at once

---
+++ webapi/Controllers/Products/ProductController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using Domain.Interfaces;
+using Domain.Models.Products;
+using Domain.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Products
+{
+    [ApiController]
+
+    [Route("achebarato/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductServices _productServices;
+
+        public ProductsController(IProductServices services)
+        {
+            _productServices = services;
+        }
+
+        [HttpGet]
+        //[EnableQuery]
+        public IQueryable<Product> GetSearch([FromQuery]ProductQueryParameters parameters)
+        {
+            return _productServices.GetAllProduct(parameters).OrderBy(x => x.Price); 
+        }
+
+        [HttpGet("{idMLBProduct}")]
+        public IActionResult GetProducyById(Guid idMLBProduct)
+        {
+            return Ok(_productServices.GetProductDTOById(idMLBProduct));
+        }
+
+        [HttpGet("cathegories")]
+        public IActionResult GetCathegories()
+        {
+            return Ok(_productServices.GetCathegories());
+        }
+
+
+        [HttpGet("cathegories/{category}")]
+        public IActionResult GetCathegories(string category)
+        {
+            return Ok(_productServices.GetProductsByCategory(category));
+        }
+        
+    }
+}
