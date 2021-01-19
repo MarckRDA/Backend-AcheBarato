7e891acd98bdfa849dc6b6eb291bd27ba4bea8b6
MarckRDA <marcos.edo132@gmail.com>
2021-01-19 08:25:24 -0300

    Updated properties

--- Domain/Models/Products/ProductDTO.cs
+++ Domain/Models/Products/ProductDTO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Domain.Models.Cathegories;
 using Domain.Models.Descriptions;
@@ -7,14 +8,17 @@ namespace Domain.Models.Products
 {
     public class ProductDTO
     {
-        public string ProductIdMLB { get; set; }
+       public Guid id_product { get; set; } 
+        public string MLBId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public string ImgLink { get; set; }
-        public List<string> Pictures { get; set; }
+        public string ThumbImgLink { get; set; }
         public string LinkRedirectShop { get; set; }
         public Cathegory Cathegory { get; set; }
-        public List<Description> Descriptions { get; set; }
-        public List<HistorycalPrice> HistorycalPrices { get; set; }
+        public List<Description> Descriptions {get; set;}
+        public List<HistorycalPrice> HistorycalṔrices {get; set;}
+        public List<string> Pictures {get; set;}
+        public string[] Tag {get; set;}
+        public bool isTrending {get; set;}
     }
 }
