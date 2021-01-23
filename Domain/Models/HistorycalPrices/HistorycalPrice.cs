295ad923f1e806227d196886a62e532e5979bd51
MarckRDA <marcos.edo132@gmail.com>
2021-01-23 10:31:29 -0300

    Changed DateOfPrice's signature

--- Domain/Models/HistorycalPrices/HistorycalPrice.cs
+++ Domain/Models/HistorycalPrices/HistorycalPrice.cs
@@ -1,13 +1,10 @@
-using System;
-
-
 namespace Domain.Models.HistorycalPrices
 {
     public class HistorycalPrice 
     {
-        public DateTime DateOfPrice { get; private set; }
+        public string DateOfPrice { get; private set; }
         public double PriceOfThatDay { get; private set; }
-        public HistorycalPrice(double priceOfThatDay, DateTime date )
+        public HistorycalPrice(double priceOfThatDay, string date )
         {
             DateOfPrice = date;
             PriceOfThatDay = priceOfThatDay;
