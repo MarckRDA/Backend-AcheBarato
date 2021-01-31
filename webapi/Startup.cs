56a165cf66566700a63490c31f07da8bc5ffcdd8
MarckRDA <marcos.edo132@gmail.com>
2021-01-31 17:23:38 -0300

    Implement dependecy injection

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -1,3 +1,4 @@
+using Cronos;
 using Domain.Common;
 using Domain.Models.Products;
 using Domain.Models.Users;
@@ -12,9 +13,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using webapi.Services.BackgroundService;
+using webapi.Services.MessagerBrokers;
 
 namespace webapi
 {
@@ -30,63 +31,61 @@ namespace webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-             services.AddCors(options =>
-            {
-                options.AddPolicy("achebarato",
-                    builder =>
-                    {
-                        builder
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .AllowAnyOrigin();
-                    }
-                );
-            });
+            services.AddCors(options =>
+           {
+               options.AddPolicy("achebarato",
+                   builder =>
+                   {
+                       builder
+                           .AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowAnyOrigin();
+                   }
+               );
+           });
 
-            services.Configure<MongoSettings>(
-                Configuration.GetSection(nameof(MongoSettings)));
 
-            services.AddScoped<IMongoSettings>(sp =>
-                sp.GetRequiredService<IOptions<MongoSettings>>().Value);
 
             services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductServices, ProductServices>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
-          
-           var mongoUrlBuilder = new MongoUrlBuilder("mongodb://root:AcheBaratoMongoDB2021!@localhost:27017");
-           var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+            services.AddScoped<IMessagerBroker, MessagerBroker>();
+            services.AddScoped<IProductBackgroundTask, ProductBackgroundTask>();
 
-           services.AddHangfire(configuration => configuration
-                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                .UseSimpleAssemblyNameTypeSerializer()
-                .UseRecommendedSerializerSettings()
-                .UseMongoStorage(mongoClient, "AcheBarato", new MongoStorageOptions
-                {
-                    MigrationOptions = new MongoMigrationOptions
-                    {
-                        MigrationStrategy = new MigrateMongoMigrationStrategy(),
-                        BackupStrategy = new CollectionMongoBackupStrategy()
-                    },
-                    Prefix = "hangfire.mongo",
-                    CheckConnection = true
-                }));
+            var mongoUrlBuilder = new MongoUrlBuilder(Configuration.GetValue<string>("MongoSettings:Connection"));
+            var mongoClient = new MongoClient(mongoUrlBuilder.ToMongoUrl());
 
-                services.AddHangfireServer(serverOptions =>
-                {
-                    serverOptions.ServerName = "Hangfire.Mongo server 1";
-                });
+            services.AddHangfire(configuration => configuration
+                 .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                 .UseSimpleAssemblyNameTypeSerializer()
+                 .UseRecommendedSerializerSettings()
+                 .UseMongoStorage(mongoClient, Configuration.GetValue<string>("MongoSettings:DatabaseName"), new MongoStorageOptions
+                 {
+                     MigrationOptions = new MongoMigrationOptions
+                     {
+                         MigrationStrategy = new MigrateMongoMigrationStrategy(),
+                         BackupStrategy = new CollectionMongoBackupStrategy()
+                     },
+                     Prefix = "hangfire.mongo",
+                     CheckConnection = true
+                 }));
+
+            services.AddHangfireServer(serverOptions =>
+            {
+                serverOptions.ServerName = "Hangfire.Mongo server 1";
+            });
 
             services.AddControllers();
-            
+
             ProductMap.Configure();
 
             UserMap.Configure();
 
 
         }
-       
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -101,7 +100,7 @@ namespace webapi
             app.UseRouting();
 
             app.UseAuthorization();
-            
+
             app.UseHangfireDashboard();
 
             app.UseHangfireServer();
@@ -116,10 +115,11 @@ namespace webapi
 
         private void InitProcess()
         {
-            var backbroundProductTasks = new ProductBackgroundTask();
-            BackgroundJob.Enqueue(() => backbroundProductTasks.PushProductsInDB());
-            BackgroundJob.Enqueue(() => backbroundProductTasks.PushTrendProductsInDB());
-            RecurringJob.AddOrUpdate(() => backbroundProductTasks.MonitorPriceProducts(), Cron.Daily(19, 00));
+            
+            BackgroundJob.Enqueue<ProductBackgroundTask>(x => x.PushProductsInDB());
+            BackgroundJob.Enqueue<ProductBackgroundTask>(x => x.PushTrendProductsInDB());
+            BackgroundJob.Enqueue<ProductBackgroundTask>(x => x.NotifyUserAboutAlarmPrice());
+            BackgroundJob.Enqueue<ProductBackgroundTask>(x => x.MonitorPriceProducts());
         }
 
 
